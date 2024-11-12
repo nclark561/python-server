c7e018f3027cec644ed351822774513de9a0fd5e
Noah Clark <noahammon00@gmail.com>
2024-11-12 16:44:31 -0700

    got all the get put post and delete post endpoints working and properly interacting with the db

--- controller/posts.py
+++ controller/posts.py
@@ -1,47 +1,62 @@
 from fastapi import APIRouter, HTTPException, Depends
-from dataAccess.posts import get_posts, get_post, create_post, update_post, delete_post
-from dataAccess.models import Post
-from dataAccess.database import get_db
+from dataAccess.posts import get_posts_da, get_post_da, create_post_da, update_post_da, delete_post_da
+from pydantic import BaseModel
+from datetime import datetime
 from sqlalchemy.orm import Session
+from dataAccess.database import get_db
+from sqlalchemy.exc import NoResultFound
 
+class PostBase(BaseModel):
+    content: str
+    
+    class Config:
+        orm_mode = True
+
+class PostResponse(PostBase):
+    id: str
+    date: datetime
 
 router = APIRouter()
 
-@router.get("/posts")
+@router.get("/posts", response_model = list[PostResponse])
 async def get_posts(db: Session = Depends(get_db)):
     print('i ran')
     try:
-        posts = get_posts(db)
+        posts = get_posts_da(db)
         if posts is None:
-            return []
+            return { posts: [] }
         return posts
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-@router.get("/posts/{id}")
-async def get_post(id: str, db: Session = Depends(get_db)):
-    post = get_post(id, db) 
+@router.get("/posts/{id}", response_model = PostResponse)
+async def get_post(id, db: Session = Depends(get_db)):
+    post = get_post_da(id, db) 
     if post is None:
         raise HTTPException(status_code=404, detail="Post not found")
     return post
 
-@router.post("/posts")
-async def create_post(post: Post, db: Session = Depends(get_db)):
-    created_post = create_post(post, db)
+@router.post("/posts", response_model = PostResponse)
+async def create_post(post: PostBase, db: Session = Depends(get_db)):
+    created_post = create_post_da(post.content, db)
     if created_post is None:
         raise HTTPException(status_code=400, detail="Failed to create post")
     return created_post
 
-@router.put("/posts/{id}")
-async def update_post(id: str, post: Post, db: Session = Depends(get_db)):
-    updated_post = update_post(id, post, db)
+@router.put("/posts/{id}", response_model = PostResponse)
+async def update_post(id: str, post: PostBase, db: Session = Depends(get_db)):
+    updated_post = update_post_da(id, post.content, db)
     if updated_post is None:
         raise HTTPException(status_code=400, detail="Failed to update post")
     return updated_post
 
 @router.delete("/posts/{id}")
 async def delete_post(id: str, db: Session = Depends(get_db)):
-    deleted_post = delete_post(id, db)
-    if deleted_post is None:
-        raise HTTPException(status_code=400, detail="Failed to delete post")
-    return deleted_post
+    try:
+        delete_post_da(id, db)
+        return { "message": "post deleted"}
+    except NoResultFound:
+        raise HTTPException(status_code=404, detail="Post with ID not found")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    return 
--- dataAccess/posts.py
+++ dataAccess/posts.py
@@ -1,16 +1,36 @@
 from dataAccess.models import Post
+from sqlalchemy.orm import Session
+from sqlalchemy.exc import NoResultFound
 
-def get_posts():
-    return Post.query.all()
+def get_posts_da(db: Session):
+    print('data access ran')
+    posts = db.query(Post).all()
+    print(posts, 'these are the posts')
+    return posts
 
-def get_post(id):
-    return Post.query.get(id)
+def get_post_da(id, db: Session):
+    return db.query(Post).filter(Post.id == id).first()
 
-def create_post(post):
-    return Post.query.create(post)
+def create_post_da(content: str, db: Session):
+    new_post = Post(content=content)
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+    return new_post
 
-def update_post(post):
-    return Post.query.update(post)
+def update_post_da(id, content: str, db: Session):
+    post = db.query(Post).filter(Post.id == id).first()
+    if post is None:
+        raise NoResultFound("post not found")
+    post.content = content
+    db.commit()
+    db.refresh(post)
+    return post
 
-def delete_post(id):
-    return Post.query.delete(id)
+def delete_post_da(id, db: Session):
+    post = db.query(Post).filter(Post.id == id).first()
+    if post is None:
+        raise NoResultFound("post not found")
+    db.delete(post)
+    db.commit()
+    return
