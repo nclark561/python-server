5596343516ccf1b4ba462ca354b9588865df242c
Noah Clark <noahammon00@gmail.com>
2024-11-13 11:29:43 -0700

    started adding user model. still need to update db

--- dataAccess/models.py
+++ dataAccess/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, String, DateTime
+from sqlalchemy import Column, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 from dataAccess.database import Base
 from datetime import datetime, timezone
 from cuid import cuid
@@ -9,3 +10,12 @@ class Post(Base):
     id = Column(String, primary_key=True, default=lambda: cuid())
     content = Column(String)
     date = Column(DateTime, default=datetime.now(timezone.utc))
+    owner_id = Column(String, ForeignKey('users.id'))
+    owner = relationship('User', back_populates='posts')
+    
+class User(Base):
+    __tablename__ = 'users'
+    
+    id = Column(String, primary_key=True, default=lambda: cuid())
+    username = Column(String)
+    posts = relationship('Post', back_populates='owner')
---
+++ dataAccess/users.py
@@ -0,0 +1,30 @@
+from dataAccess.models import User
+from sqlalchemy.orm import Session
+from sqlalchemy.exc import NoResultFound
+
+def get_user_da(id: str, db: Session):
+    return db.query(User).filter(User.id == id).first()
+
+def create_user_da(username: str, db: Session):
+    new_user = User(username=username)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+def update_user_da(id: str, username: str, db: Session):
+    updated_user = db.query(User).filter(User.id == id).first()
+    if updated_user is None:
+        raise NoResultFound("user not found")
+    updated_user.username = username
+    db.commit()
+    db.refresh(updated_user)
+    return updated_user
+
+def delete_user_da(id: str, db: Session):
+    deleted_user = db.query(User).filter(User.id == id).first()
+    if deleted_user is None:
+        raise NoResultFound('user not found')
+    db.delete(deleted_user)
+    db.commit()
+    return 
