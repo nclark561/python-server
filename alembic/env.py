30f457debdcab3191d158b0f7a6a1601ee43b0dc
Noah Clark <noahammon00@gmail.com>
2024-11-16 21:24:24 -0700

    got alembic updated to fix the db

--- alembic/env.py
+++ alembic/env.py
@@ -1,5 +1,5 @@
 import os
-from sqlalchemy import engine_from_config
+from dataAccess.database import engine
 from sqlalchemy import pool
 from dotenv import load_dotenv
 from alembic import context
@@ -57,11 +57,7 @@ def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    connectable = engine
 
     with connectable.connect() as connection:
         context.configure(
